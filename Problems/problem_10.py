64b66770200ee586d40ff2e71627a6f8f1c93bff
wuihee <wuihee@gmail.com>
2017-05-11 20:00:42 -0700

    problem 10

--- Problems/problem_10.py
+++ Problems/problem_10.py
@@ -5,12 +5,12 @@ def sieve(num):
     """Uses the Sieve of Erastosthenes to calculate all primes up to 'num'."""
     A = [True for i in range(num)]
 
-    for i in range(2, int(num ** 0.5)):
+    for i in range(2, int(num ** 0.5)):  # Square root because j starts at i ** 2.
         if A[i] is True:
-            for j in range(i ** 2, len(A), i):
-                A[j] = False
+            for j in range(i ** 2, len(A), i):  # The first multiple is i ** 2.
+                A[j] = False  # Change all multiples to False
 
-    return [i for i in range(len(A)) if A[i] is True][2:]  # Remove 0 and 1
+    return [i for i in range(len(A)) if A[i] is True][2:]  # Remove 0 and 1.
 
 
 print(sum(sieve(2000000)))
